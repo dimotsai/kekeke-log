aeef7a3b9a32d4ff4f4310ceb202ce189f88b289
Chih-Hao Tsai <a968574123@hotmail.com>
2016-11-10 12:48:23 +0800

    Fix basename problem (?)

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './app/containers/App';
 import configureStore from './app/store/configureStore';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, useRouterHistory} from 'react-router';
+import {createHistory} from 'history';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 import './index.scss';
 
+const history = useRouterHistory(createHistory)({
+  basename: '/cq'
+});
+
 const store = configureStore();
 
 render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <Router history={browserHistory}>
+      <Router history={history}>
         <Route path="/" component={App}/>
       </Router>
     </MuiThemeProvider>
